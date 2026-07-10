ac8b95114806cb02dc804837cc0d96f7b2e7157b
André Moreira <andre.vsunimed@gmail.com>
2026-07-10 18:51:50 -0300

    add metodo reset password

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -115,37 +115,109 @@ router.get('/', async (req, res) => {
  *         description: Erro interno do servidor.
  */
   router.post('/novo-usuario', async (req, res) => {
-    const { usuario_nome,
-            usuario_usuario,
-            usuario_senha,
-            usuario_email,
-            usuario_nascimento,
-            usuario_cidade,
-            usuario_uf,          
-            usuario_status,
-            usuario_empresaid,
-            usuario_filialid,
-            usuario_cep,
-            usuario_lat,
-            usuario_long
-          } = req.body;  
-       try {
-        const bcrypt = require("bcrypt");
-        const senhaHash = await bcrypt.hash(usuario_senha, 10);
-        const usuarios = await db.query('INSERT INTO usuarios (usuario_nome, usuario_usuario, usuario_senha, usuario_email, usuario_nascimento, usuario_cidade, usuario_uf, usuario_status, usuario_empresaid, usuario_filialid, usuario_cep, usuario_lat, usuario_long) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        [usuario_nome, usuario_usuario, senhaHash, usuario_email, usuario_nascimento, usuario_cidade, usuario_uf, usuario_status, usuario_empresaid, usuario_filialid, usuario_cep, usuario_lat, usuario_long]);  
-        res.status(201).json({
-            sucesso: true,
-            mensagem: 'Usuário cadastrado com sucesso.',
-            usuario_id: usuarios[0].insertId
+    try {
+      const { usuario_nome,
+              usuario_usuario,
+              usuario_senha,
+              usuario_email,
+              usuario_nascimento,
+              usuario_cidade,
+              usuario_uf,          
+              usuario_status,
+              usuario_empresaid,
+              usuario_filialid,
+              usuario_cep,
+              usuario_lat,
+              usuario_long
+            } = req.body;
+
+      // Validar campos obrigatórios
+      if (!usuario_nome || !usuario_usuario || !usuario_senha || !usuario_email) {
+        return res.status(400).json({
+            sucesso: false,
+            mensagem: 'Nome, usuário, senha e email são obrigatórios.'
         });
-      } catch (erro) {   
-          console.error(erro);
-          res.status(500).json({
-              sucesso: false,
-              mensagem: 'Erro ao buscar usuarios.',
-              erro: erro.message
-          });
       }
+
+      // Verificar se usuário já existe
+      const [existe] = await db.query(`
+            SELECT usuario_id
+            FROM usuarios
+            WHERE UPPER(usuario_nome)=UPPER(?)
+               OR UPPER(usuario_email)=UPPER(?)
+               OR UPPER(usuario_usuario)=UPPER(?)
+        `,
+        [
+            usuario_nome,
+            usuario_email,
+            usuario_usuario
+        ]);
+
+      if (existe.length > 0) {
+        return res.status(409).json({
+            sucesso: false,
+            mensagem: "Usuário já cadastrado"
+        });
+      }    
+      
+      const bcrypt = require("bcrypt");
+      const senhaHash = await bcrypt.hash(usuario_senha, 10);
+      const [result] = await db.query('INSERT INTO usuarios (usuario_nome, usuario_usuario, usuario_senha, usuario_email, usuario_nascimento, usuario_cidade, usuario_uf, usuario_status, usuario_empresaid, usuario_filialid, usuario_cep, usuario_lat, usuario_long) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      [usuario_nome, usuario_usuario, senhaHash, usuario_email, usuario_nascimento, usuario_cidade, usuario_uf, usuario_status, usuario_empresaid, usuario_filialid, usuario_cep, usuario_lat, usuario_long]);  
+      
+      // Vincula perfil padrão (3)
+      await db.query(`
+          INSERT INTO perfil_x_usuario(
+              perfil_x_usuario_usuarioid,
+              perfil_x_usuario_perfilid
+          )
+          VALUES (?,3)
+      `,
+      [
+          result.insertId
+      ]);
+      
+      res.status(201).json({
+          sucesso: true,
+          mensagem: 'Usuário cadastrado com sucesso.',
+          usuario_id: result.insertId
+      });
+    } catch (erro) {   
+        console.error(erro);
+        res.status(500).json({
+            sucesso: false,
+            mensagem: 'Erro ao cadastrar usuário.',
+            erro: erro.message
+        });
+    }
   })
-      module.exports = router;
+   // Reset password
+   /**
+ * @swagger  
+* /reset-password:
+ *   post:
+ *     summary: resetar senha do usuário
+ *     tags:
+ *       - Usuarios
+ *     responses:
+ *       200:
+ *         description: resetar senha do usuário
+ */
+
+  router.post('/reset-password', async (req, res) => {
+    try {
+      const { usuario_email } = req.body;
+      const bcrypt = require("bcrypt");
+      const novaSenha = Math.random().toString(36).slice(-8);
+      const senhaHash = await bcrypt.hash(novaSenha, 10); 
+      result = await db.query('UPDATE usuarios SET usuario_senha = ? WHERE usuario_email = ?', [senhaHash, usuario_email]);  
+    } catch (erro) {
+      console.error(erro);
+      res.status(500).json({
+        sucesso: false,
+        mensagem: 'Erro ao resetar senha.',
+        erro: erro.message
+      });
+    }
+  });
+  module.exports = router;
